727e02be19b048b683fccf4766b7ec9e434437f3
Anderson Felipe <anderson_fpv@outlook.com>
2017-06-03 10:57:46 -0300

    alteração

--- Calculadora.java
+++ Calculadora.java
@@ -10,5 +10,21 @@ public class Calculadora {
         
     private double numero1;
     private double numero2;
-    }
+    
+	public Calculadora(){
+		numero1 = numero2 = 0;
+	}
+	public int getNumero1(){
+		return numero1;
+	}		
+	public void setNumero1(double numero){
+		this.numero1 = numero;
+	}
+	
+	public int getNumero2(){
+		return numero2;
+	}		
+	public void setNumero2(double numero){
+		this.numero2 = numero;
+	}
 }
